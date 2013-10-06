217c5fc1c95d7cbb092bde8419dd9c353cf30a7b
Guy Pursey <guypursey@gmail.com>
2013-10-06 09:31:54 +0100

    Outsourced Lisp interpretation functions to separate file and moved scripting in console.html to bottom of document body.

---
+++ lisp_interpreter.js
@@ -0,0 +1,115 @@
+var lisp_fns = {
+		"+": function (args) {
+			var	rtn = 0; // CLISP returns 0 if `+` is given no arguments.
+			while (args.length) {
+				rtn += args.shift();
+			};
+			return rtn;
+		},
+		"-": function (args) {
+			var rtn = args.shift();
+			while (args.length) {
+				rtn -= args.shift();
+			};
+			return rtn;
+		},
+		"*": function (args) {
+			var rtn = 1; // CLISP returns 1 if `*` is given no arguments.
+			while (args.length) {
+				rtn *= args.shift();
+			};
+			return rtn;
+		},
+		"/": function (args) {
+			var rtn = args.shift();
+			while (args.length) {
+				rtn /= args.shift();
+			}
+			return rtn;
+		}
+	},
+
+	separate_terms = function (input) {
+		var str = input,
+			arr = [],
+			rst = "",
+			rex = /(\s+)|(")|(\()|(\))|(.$)/g,
+			begin_term = 0,
+			end_term = 0,
+			paren_level = 0,
+			quote_level = false,
+			rfn = function ($m, $1, $2, $3, $4, $5, offset, whole_string) {
+				var out;
+				if ($2 && !paren_level) {
+					console.log(quote_level);
+					quote_level = !quote_level;
+					if (quote_level) {
+						begin_term = offset;
+					} else {
+						end_term = offset + $m.length;
+						out = whole_string.slice(begin_term, end_term);
+						console.log(end_term);
+						console.log(out);
+						begin_term = end_term;
+					}
+				} else if ($3 && !quote_level) {
+					paren_level += 1;
+					if (!paren_level) {
+						begin_term = offset;
+					}
+				} else if ($4 && !quote_level) {
+					paren_level -= 1;
+					if (!paren_level) {
+						end_term = offset + $m.length;
+						out = whole_string.slice(begin_term, end_term);
+						begin_term = end_term;
+					}
+				} else if ($1 && !paren_level && !quote_level) {
+					end_term = offset;
+					out = whole_string.slice(begin_term, end_term);
+					begin_term = end_term + $m.length;
+				} else if ($5 && !quote_level) {
+					end_term = whole_string.length;
+					out = whole_string.slice(begin_term, end_term);
+					begin_term = end_term + $m.length;
+				}
+				
+				if (out) { arr.push(out); };
+				return $m;
+			};
+
+		rst = str.replace(rex, rfn);
+		return arr;
+	},
+
+	evaluate_term = function (term) {
+		var rtn,
+			arr,
+			fun,
+			arg = [];
+		if (/\D/g.test(term)) {
+			if (/^\(.*\)$/.test(term)) {
+				if (term.slice(1, -1)) {
+					arr = separate_terms(term.slice(1, -1));
+					fun = arr.shift();
+					while (arr.length) {
+						arg.push(evaluate_term(arr.shift()));
+					}
+					if (lisp_fns.hasOwnProperty(fun)) {
+						rtn = lisp_fns[fun](arg);
+					} else {
+						// TODO: What if there is no such function?
+					}
+				} else {
+					// If parentheses are empty.
+					rtn = "NIL";
+				}
+			} else if (/^".*"$/g.test(term)) {
+				console.log(term);
+				rtn = term;
+			}
+		} else {
+			rtn = +term;
+		}
+		return rtn;
+	};
