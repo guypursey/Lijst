21f1e13c2ba12c3169fd53ddd997ecd3991d0cba
Guy Pursey <guypursey@gmail.com>
2013-10-25 23:05:52 +0100

    Created reference to a test element as a local element for the IIFE.

--- node_tdd.js
+++ node_tdd.js
@@ -29,22 +29,21 @@ while (test_units.length) {
 		console.log("Testing: " + test_unit);
 		
 		while (test_array.length) {
-			(function () {
-				var test_element = test_array.shift(),
-					callback_each_term = function (input, result) {
-							var expected_result = test_element.r.shift();
-							if (check_flag("v")) {
-								console.log("> " + input);
-								console.log("Expecting: " + expected_result);
-								console.log(result);
-								console.log((expected_result === result) ? "PASSED" : "FAILED");
-							}
-							if (expected_result !== result) {
-								failure_stack.push(((test_array.length)) + ": " + result);
-							}
-						},
+			(function (test_element) {
+				var callback_each_term = function (input, result) {
+						var expected_result = test_element.r.shift();
+						if (check_flag("v")) {
+							console.log("> " + input);
+							console.log("Expecting: " + expected_result);
+							console.log(result);
+							console.log((expected_result === result) ? "PASSED" : "FAILED");
+						}
+						if (expected_result !== result) {
+							failure_stack.push(((test_array.length)) + ": " + result);
+						}
+					},
 					rst = lisp_interpreter.process_input(test_element.t, callback_each_term);
-			})();
+			})(test_array.shift());
 		};
 		
 		console.log((100 - (test_unit_worth * failure_stack.length)) + "% PASSED");
