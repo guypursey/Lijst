73351784bfa6afabd5d09bfbff54737f7b453510
Guy Pursey <guypursey@gmail.com>
2013-10-27 09:56:12 +0000

    Refactored node_tdd.js replacing while and for loops with Array's forEach method and callbacks.

--- node_tdd.js
+++ node_tdd.js
@@ -1,4 +1,4 @@
-var lisp_interpreter = require("./lisp_interpreter.js").init( function () { console.log("Interpreter loaded..."); } ),
+var lisp_interpreter = require("./lisp_interpreter.js").init(function () { console.log("Interpreter loaded..."); }),
 	test_folder = "./test_units/",
 	test_units = require(test_folder + "_test_units"),
 	check_flag = (function () {
@@ -12,8 +12,8 @@ var lisp_interpreter = require("./lisp_interpreter.js").init( function () { cons
 					i -= 1;
 					if (isFlag.test(flags[i])) {
 						cache[whichFlag] = true;
-					};
-				};
+					}
+				}
 				return (cache[whichFlag] = cache[whichFlag] || false);
 			})();
 		};
@@ -23,43 +23,41 @@ var lisp_interpreter = require("./lisp_interpreter.js").init( function () { cons
 			l = failure_stack.length;
 		console.log((100 - (test_unit_worth * l)) + "% PASSED");
 		console.log(l ? (l + " FAILURES:") : " ");
-		for (; i < l; i += 1) {
+		failure_stack.forEach(function (item) {
 			console.log(failure_stack[i]);
-		}
+		});
 		console.log(" ");
+		return true;
 	};
 
-while (test_units.length) {
-	(function () {
-		var test_unit = test_units.shift(),
-			test_array = require(test_folder + test_unit),
+test_units.forEach(function(test_unit) {
+		var test_array = require(test_folder + test_unit),
 			test_unit_worth = 100 / test_array.length,
 			failure_stack = [];
 		
 		console.log("Testing: " + test_unit);
 		
-		while (test_array.length) {
-			(function (test_element) {
-				var callback_each_term = function (input, result) {
-						var expected_result = test_element.r.shift();
-						if (check_flag("v")) {
-							console.log("> " + input);
-							console.log("Expecting: " + expected_result);
-							console.log(result);
-							console.log((expected_result === result) ? "PASSED" : "FAILED");
-						}
-						if (expected_result !== result) {
-							failure_stack.push(((test_array.length)) + ": " + result);
-						}
-					},
-					rst = lisp_interpreter.process_input(test_element.t, callback_each_term);
-			})(test_array.shift());
-		};
+		test_array.forEach(function (test_element, index, array) { 
+			var result_index = 0;
+			lisp_interpreter.process_input(test_element.t, function (input, result) {
+				var expected_result = test_element.r[result_index];
+				if (check_flag("v")) {
+					console.log("> " + input);
+					console.log("Expecting: " + expected_result);
+					console.log(result);
+					console.log((expected_result === result) ? "PASSED" : "FAILED");
+				}
+				if (expected_result !== result) {
+					failure_stack.push(
+						"TEST " + index + " FAILED: " + test_element.c +
+						"\nEXPECTED RESULT " + result_index + ": " + test_element.r[result_index] +
+						"\nRECEIVED RESULT " + result_index + ": " + result
+					);
+				}
+				result_index += 1;
+			});
+		});
 		
 		report_on_failure_stack(test_unit_worth, failure_stack);
 		
-	})();
-};
-
-
-
+});
